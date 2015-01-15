5e2999028edd5a2f50cbd9aa604df4d1a2a0d904
Yuk Lai Suen <yuklai@hothouselabs.com>
2015-01-14 17:44:33 -0800

    Remove ODLog

--- ODReveal.m
+++ ODReveal.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 OnDotDot. All rights reserved.
 //
 
-#import "ODLog.h"
 #import "ODReveal.h"
 #import <dlfcn.h>
 
@@ -34,14 +33,14 @@
     NSString *revealLibName = @"libReveal";
     NSString *revealLibExtension = @"dylib";
     NSString *dyLibPath = [[NSBundle mainBundle] pathForResource:revealLibName ofType:revealLibExtension];
-    ODLog(@"Loading dynamic library: %@", dyLibPath);
+    NSLog(@"Loading dynamic library: %@", dyLibPath);
     
     _revealLib = dlopen([dyLibPath cStringUsingEncoding:NSUTF8StringEncoding], RTLD_NOW);
     
     if (_revealLib == NULL)
     {
         char *error = dlerror();
-        ODLog(@"dlopen error: %s", error);
+        NSLog(@"dlopen error: %s", error);
     }
     else
     {
@@ -59,13 +58,13 @@
         [[NSNotificationCenter defaultCenter] postNotificationName:@"IBARevealRequestStop" object:self];
         if (dlclose(_revealLib) == 0)
         {
-            ODLog(@"Reveal library unloaded");
+            NSLog(@"Reveal library unloaded");
             _revealLib = NULL;
         }
         else
         {
             char *error = dlerror();
-            ODLog(@"Reveal library could not be unloaded: %s", error);
+            NSLog(@"Reveal library could not be unloaded: %s", error);
         }
     }
 #endif
